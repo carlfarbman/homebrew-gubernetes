5a7a3761bbf88497730a577d2d83a4cb2e6bcc00
tdelph <tdelph@newrelic.com>
2024-05-21 23:18:22 -0700

    update relase

--- Formula/gubernetes.rb
+++ Formula/gubernetes.rb
@@ -1,14 +1,14 @@
 class Gubernetes < Formula
-    desc "Turn any word into a k8s abbreviation"
-    homepage "https://github.com/carlfarbman/gubernetes"
-    url "https://github.com/carlfarbman/gubernetes/releases/download/v1.0.0/gubernetes"
-    sha256 "0c1f74743cdee408e21dfa1ed9911aa1a789ff0df8798ace2ce2b2bbe6b88877"
-  
-    def install
-      bin.install "gubernetes"
-    end
-  
-    test do
-      system "#{bin}/gubernetes", "test"
-    end
+  desc "Turn any word into a k8s abbreviation"
+  homepage "https://github.com/carlfarbman/gubernetes"
+  url "https://github.com/carlfarbman/gubernetes/releases/download/v1.0.1/gubernetes"
+  sha256 "5c9f21c6f67d7e801e475176c67416433b9e5888ba351ee72e0d0395e855eb5f"
+
+  def install
+    bin.install "gubernetes"
   end
+
+  test do
+    system "#{bin}/gubernetes", "test"
+  end
+end
